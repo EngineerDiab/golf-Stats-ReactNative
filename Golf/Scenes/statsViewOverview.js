0114bdbbab464561651faae014c1d69afe2167ca
Mathieu Diab <mathieudiab@Mathieus-MacBook-Pro.local>
2016-07-25 15:18:49 -0400

    all functions for stat calculations have been added. need to fix left-centre-right tendency in stats. the finish UI

--- Golf/Scenes/statsViewOverview.js
+++ Golf/Scenes/statsViewOverview.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import {Actions} from 'react-native-router-flux';
 var Slider = require('react-native-slider');
 import Button from 'apsl-react-native-button';
-import {PieChart} from 'react-native-ios-charts';
+import {PieChart, HorizontalBarChart} from 'react-native-ios-charts';
 var _ = require('lodash');
 import realm from './realm';
 
@@ -44,19 +44,140 @@ export default class statsViewOverview extends Component {
     return ((trueCounter/GIRArray.length)*100)
   }
 
+  _calculateScoringMarkers(){
+    var getArray = realm.objects('Hole').filtered(`date == "${this.props.date}" AND round == "${this.props.round}"`).slice('0')
+    var scoringMarkerArray = [0,0,0,0,0,0,0]
+    for(var i=0; i < getArray.length; i++){
+       var theValue = getArray[i].par - (getArray[i].fullStroke + getArray[i].halfStroke + getArray[i].puts + getArray[i].penalties)
+       console.log(theValue)
+       switch(theValue){
+           case 2:
+              scoringMarkerArray[0]++;
+              break;
+           case 1:
+              scoringMarkerArray[1]++;
+              break;
+           case 0:
+              scoringMarkerArray[2]++;
+              break;
+           case -1:
+              scoringMarkerArray[3]++;
+              break;
+           case -2:
+              scoringMarkerArray[4]++;
+              break;
+           case -3:
+              scoringMarkerArray[5]++;
+              break;
+           default:
+              scoringMarkerArray[6]++;
+        }
+      }
+      console.log(scoringMarkerArray)
+      return scoringMarkerArray
+  }
+
+    _calculateScoringAverage(parValue){
+      var getArray = realm.objects('Hole').filtered(`date == "${this.props.date}" AND round == "${this.props.round}" AND par== ${parValue}`).slice('0')
+      var total = 0
+      for(var i=0; i < getArray.length; i++){
+        total = total + getArray[i].fullStroke + getArray[i].halfStroke + getArray[i].puts + getArray[i].penalties
+      }
+      console.log(total/getArray.length)
+      return (total/getArray.length)
+    }
+
+    _calculateAvgHoleProx(){
+      var getArray = realm.objects('Hole').filtered(`date == "${this.props.date}" AND round == "${this.props.round}"`).slice('0')
+      var total = 0
+      for(var i=0; i < getArray.length; i++){
+        total = total + getArray[i].firstPutDistance
+      }
+      console.log(total/getArray.length)
+      return (total/getArray.length)
+    }
+
+    _calculatePutsPerRound(){
+      var getArray = realm.objects('Hole').filtered(`date == "${this.props.date}" AND round == "${this.props.round}"`).slice('0')
+      var total = 0
+      for(var i=0; i < getArray.length; i++){
+        total = total + getArray[i].puts
+      }
+      console.log(total)
+      return (total)
+    }
+
+    _calculateNumberOf123Puts(putValue){
+      return(realm.objects('Hole').filtered(`date == "${this.props.date}" AND round == "${this.props.round}" AND par== ${putValue}`).slice('0').length)
+    }
+
+    _calculateUpAndDown(){
+      return(realm.objects('Hole').filtered(`date == "${this.props.date}" AND round == "${this.props.round}" AND halfStroke >= ${1} AND puts==${1}`).slice('0').length)
+    }
+
+
 
   render() {
+    //Scoring Average
+    var valueSCM = this._calculateScoringMarkers()
+    var scoringAvg3 = this._calculateScoringAverage(3)
+    var scoringAvg4 = this._calculateScoringAverage(4)
+    var scoringAvg5 = this._calculateScoringAverage(5)
+    //Accuracy
     var valueGIR = this._calculateGIRPercentage()
+    var avgHoleProx = this._calculateAvgHoleProx()
+    //Putting
+    var totalPutsPerRound = this._calculatePutsPerRound()
+    var totalPutsPerRoundAvg = totalPutsPerRound/realm.objects('Hole').filtered(`date == "${this.props.date}" AND round == "${this.props.round}"`).slice('0').length
+    var zeroPuts = this._calculateNumberOf123Puts(0)
+    var onePuts = this._calculateNumberOf123Puts(1)
+    var twoPuts = this._calculateNumberOf123Puts(2)
+    var threePuts = this._calculateNumberOf123Puts(3)
+    var overPuts = realm.objects('Hole').filtered(`date == "${this.props.date}" AND round == "${this.props.round}"`).slice('0').length - (zeroPuts+onePuts+twoPuts+threePuts)
+    //Up and Down
+    var upAndDown = this._calculateUpAndDown()
+    console.log(upAndDown)
 
-    const config = {
+    const configGIR = {
       dataSets: [{
         values: [valueGIR,100-valueGIR],
         colors: ['#F27852', '#1a9274'],
         //label: 'GIR'
       }],
       backgroundColor: 'transparent',
+      pinchZoomEnabled: false,
+      doubleTapToZoomEnabled: false,
       //labels: ['GIR']
+      //drawHoleEnabled: true,
+      // centerText: 'GIR',
+      // drawSliceTextEnabled: false,
+      usePercentValuesEnabled: true,
+      maxAngle: 360
     };
+    const configSCM = {
+      dataSets: [{
+        values: [0,1,2,3,4,5,18],
+        valueTextFontSize: 12
+      }],
+      backgroundColor: 'transparent',
+      pinchZoomEnabled: false,
+      doubleTapToZoomEnabled: false,
+      labels: ['Eagle', 'Birdie', 'Par', 'Bogie', 'Double Bogie', 'Triple Bogie', 'Quad+'],
+      showLegend: false,
+      minOffset: 20,
+      xAxis: {
+        axisLineWidth: 0,
+        position: 'bottom'
+      },
+      leftAxis: {
+        spaceTop: 0.18,
+      },
+      rightAxis: {
+        enabled: false,
+        drawGridLines: false
+      }
+    };
+
     return (
       <ScrollView style={styles.container1}>
         <View style={styles.containerRow}>
@@ -70,7 +191,7 @@ export default class statsViewOverview extends Component {
           </View>
           <View style={{justifyContent: 'space-around'}}>
             <View style={{alignSelf: 'center'}}>
-              <PieChart config={config} style={styles.chart}/>
+              <PieChart config={configGIR} style={styles.chart}/>
             </View>
           </View>
         </View>
@@ -87,7 +208,7 @@ export default class statsViewOverview extends Component {
           </View>
           <View style={{justifyContent: 'space-around'}}>
             <View style={{alignSelf: 'center'}}>
-              <PieChart config={config} style={styles.chart}/>
+              <HorizontalBarChart config={configSCM} style={styles.chart}/>
             </View>
           </View>
         </View>
@@ -104,7 +225,7 @@ export default class statsViewOverview extends Component {
           </View>
           <View style={{justifyContent: 'space-around'}}>
             <View style={{alignSelf: 'center'}}>
-              <PieChart config={config} style={styles.chart}/>
+              <PieChart config={configSCM} style={styles.chart}/>
             </View>
           </View>
         </View>
